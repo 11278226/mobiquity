ec660d814d9e171845ac7aaf7315c71ef8cd4464
Diarmuid O'Keeffe <diarmuido@uber.com>
2022-01-10 16:11:48 +0100

    Added Empty project and workspace

---
+++ Mobiquity/Mobiquity/MobiquityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobiquityApp.swift
+//  Mobiquity
+//
+//  Created by Diarmuid O'Keeffe on 10/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MobiquityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
